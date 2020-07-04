4a3e05c298b6cd08d5915266963f4708881bc688
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-07-04 18:49:28 +0530

    React App

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+    <React.Fragment>
+        <h1>Welcome to home page</h1>
+    </React.Fragment>
+    );
+}
+
+export default Home;
---
+++ src/Components/Work/Work.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Work = () => {
+    return(
+    <React.Fragment>
+        <h1>Welcome to Work page</h1>
+    </React.Fragment>
+    );
+}
+
+export default Work;
