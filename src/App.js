b585024c3abd9d14729db52c193bd6727433986b
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-07-05 13:03:08 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Home from './Components/Home/Home';
 import Work from './Components/Work/Work';
 import Contact from './Components/Contact/Contact';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <Home/>
-      <Work/>
-      <Contact/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/work"  component={Work}/>
+        <Route path="/contact"  component={Contact}/>
+        <Redirect to="/"/>
+      </Switch>
     </React.Fragment>
   );
 }
