fe810cc0cd054c6b436aac06fedf9b7597ef2670
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2020-07-08 23:45:46 +0530

    Footer bar

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,26 +4,16 @@ import { Link, NavLink } from 'react-router-dom';
 const Footer = () => {
     return(
         <Fragment>
+            <section id="footer">
             <div className="container-fluid nav_bg">
                 <div className="row">
-                        <div className="col-10 mx-auto">
-                                    <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                                        <li className="nav-item">
-                                            <NavLink exact activeClassName="bar_active" className="nav-link" to="/">Home</NavLink>
-                                        </li>
-                                        <li className="nav-item">
-                                            <NavLink activeClassName="bar_active" className="nav-link" to="/work">Work</NavLink>
-                                        </li>
-                                        <li className="nav-item">
-                                            <NavLink activeClassName="bar_active" className="nav-link" to="/about">About</NavLink>
-                                        </li>
-                                        <li className="nav-item">
-                                            <NavLink activeClassName="bar_active" className="nav-link" to="/contact">Contact</NavLink>
-                                        </li>
-                                    </ul>
-                                </div>
+                    <div className="col-10 mx-auto">
+                            <span><a href="https://www.github.com">Github</a></span>
+                            <span><a href="https://www.linkedin.com/">linkedin</a></span>
                         </div>
-                    </div>
+                </div>
+            </div>
+            </section>
         </Fragment>
     );
 }
