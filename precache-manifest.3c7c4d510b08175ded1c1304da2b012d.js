e35f6a31b8e7b9496c5dfff5ea825716d2cb80b1
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-04-24 17:31:17 +0530

    Updates

---
+++ precache-manifest.3c7c4d510b08175ded1c1304da2b012d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe4cfc4821b225ead12d80f4d5715ff",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "01432ab6c14043893ba5",
+    "url": "/profile/static/css/2.c6baaf7d.chunk.css"
+  },
+  {
+    "revision": "3104bf70bf741e0ffc48",
+    "url": "/profile/static/css/main.0c6a0fa5.chunk.css"
+  },
+  {
+    "revision": "01432ab6c14043893ba5",
+    "url": "/profile/static/js/2.13d4b3f6.chunk.js"
+  },
+  {
+    "revision": "71697ff5f0e375d867d41e78726c5926",
+    "url": "/profile/static/js/2.13d4b3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3104bf70bf741e0ffc48",
+    "url": "/profile/static/js/main.f9795967.chunk.js"
+  },
+  {
+    "revision": "5076af70b647b2393d8e",
+    "url": "/profile/static/js/runtime-main.8771bb24.js"
+  },
+  {
+    "revision": "cf117cd6183175cf7e78911eeb57ed6b",
+    "url": "/profile/static/media/about4.cf117cd6.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/profile/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/profile/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/profile/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/profile/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/profile/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/profile/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "6e860d67cd75d7aed1438aaa2d8888c0",
+    "url": "/profile/static/media/git.6e860d67.svg"
+  },
+  {
+    "revision": "dbe344130a2f6cd4215a37aba15de099",
+    "url": "/profile/static/media/home3.dbe34413.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/profile/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/profile/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/profile/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/profile/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/profile/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8d1c7cca1c4f7bef64c351620731a32e",
+    "url": "/profile/static/media/link.8d1c7cca.svg"
+  },
+  {
+    "revision": "34a45c38c3ade3bd99634be41be2efcf",
+    "url": "/profile/static/media/linkden2.34a45c38.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/profile/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/profile/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/profile/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/profile/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/profile/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "1904d20c10b3f50ea560b34671c7d70c",
+    "url": "/profile/static/media/pngtree-download-arrow.1904d20c.jpg"
+  }
+]);
