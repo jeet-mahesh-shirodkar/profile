d248a608320d89c537ac8be3edb613878d98d33f
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-04-24 16:37:39 +0530

    Descripton change

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -17,12 +17,12 @@ const Home = () => {
                     <div className="col-10 mx-auto">
                         <div className="row">
                             <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                                <h1>Ciao Team ! <strong className="brand-name">Sara </strong> here.</h1>
-                                    <h2 className="my-3">I 💖 Turmeric Milk with Honey 😋 and I'm a JS Dev 🤓.
-                                         Peace IN.   
+                                <h1>Ciao Team ! <strong className="brand-name">Jeet </strong> here.</h1>
+                                    <h2 className="my-3">I'm a JS Dev 🤓 and I 💖 Turmeric Milk with Honey 😋.
+                                         Peace IN.  
                                     </h2>
-                                        <div className="mt-3">
-                                            Find More <NavLink to="/about" className="btn-get-started">About Me</NavLink>
+                                        <div className="mt-3" style={{zIndex: "5"}}>
+                                            <NavLink to="/about" className="btn-get-started">About Me</NavLink>
                                         </div>
                             </div>
                             <div className="col-lg-6 order-1 order-lg-2 header-img">
