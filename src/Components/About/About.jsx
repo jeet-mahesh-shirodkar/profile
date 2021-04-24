e924acf1f70d17cb8d2ca86ec0bacfdcac953ac4
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-04-24 16:24:25 +0530

    Added Skill-tree section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,8 +1,96 @@
-import React from "react";
-import about from '../../Images/about1.svg';
-import AboutD3 from "../Animations/AboutAnimation/AboutAnimationD3.js";
+import React, { useEffect ,useState,useRef} from "react";
+import about from '../../Images/about4.jpg';
+import Tree from 'react-d3-tree';
+import './About.css';
 
+const debugData = [
+    {
+      name: "Click Node",
+      children: [
+        {
+          name: "HTML",
+        },
+        {
+          name: "CSS",
+          children: [
+            {
+              name: "Framework",
+              children: [
+                {
+                  name: "BootStrap"
+                },
+                {
+                  name: "Materialized UI"
+                },
+                {
+                  name : "Semantic UI"
+                }
+              ],
+            },
+            {
+              name: "Responsive Web"
+            },
+            {
+              name:"Preprocessors",
+              children:[
+                {
+                  name:"sass"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "JavaScript",
+          children: [
+            {
+              name: "Library & Framework",
+              children: [
+                {
+                  name: "React"
+                },
+                {
+                  name: "Leaflet"
+                }  
+              ],
+            },
+            {
+              name: "Package Manager",
+              children: [
+                {
+                  name: "NPM"
+                },
+                {
+                  name: "Yarn"
+                }  
+              ]
+            },
+            {
+              name:"Testing",
+              children:[
+                {
+                  name:"Jest"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  
 const About = () => {
+
+    const [translate, settranslate] = useState({ x: 0, y: 0 });
+    const treeContainer = useRef(null);
+
+    useEffect(()=>{
+        const dimensions = treeContainer.current.getBoundingClientRect();
+        settranslate({
+            x: 60/2,
+            y: dimensions.height / 2
+          })},[]);
+
     return(
     <React.Fragment>
         <section id="header" className="d-flex align-items-center">
@@ -20,7 +108,7 @@ const About = () => {
                             I was taught web development and designing by working in industrial projects and many times, myself 🤓.
                             I also have Bachelor of Engineering in Electronic from Mumbai University 📚.
                             </p> 
-                            <p>I believe teaching and accepting new logics, Is key to happiness 😉.</p>
+                            <p>I believe learning and accepting new logics, Is key to happiness 😉.</p>
                             </div> 
                             <div className="col-lg-6 order-1 order-lg-2 header-img">
                                 <div className="image about">
@@ -32,13 +120,11 @@ const About = () => {
                 </div>
             </div>
         </section>
-        <div className="down-about">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-10 mx-auto">
-                        <AboutD3/>
-                    </div>
-                </div>
+        <div className="skill__nodes">
+            <h3 className="skill__heading">Skill-Tree.</h3>
+                <p>Dragable Component created using d3js-tree source. Sometimes I use D3js or Chartjs for representing the Data 📈.</p>
+            <div id="treeWrapper" className= "treeWrapper_class" style={{ width: '100%', height: '30em' }} ref={treeContainer}>
+                <Tree data={debugData} translate={translate} orientation={'horizontal'}/>
             </div>
         </div>
     </React.Fragment>
