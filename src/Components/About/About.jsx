6bd8e07256c5c752e7f8e5fb40ecac54801ad313
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2025-02-17 12:58:43 +0530

    D3 data changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -51,7 +51,21 @@ const debugData = [
                 },
                 {
                   name: "Leaflet"
-                }  
+                },{
+                  name :"Redux",
+                  children:[
+                    {
+                      name:"react-redux"
+                    },{
+                      name:"redux-toolkit"
+                    },{
+                      name:"RTK-query"
+                    }
+                  ]},{
+                  name:"D3.js"
+                },{
+                  name:"E-charts"
+                } 
               ],
             },
             {
@@ -70,8 +84,51 @@ const debugData = [
               children:[
                 {
                   name:"Jest"
+                },{
+                  name:"React-testing-library"
                 }
               ]
+            }
+          ]
+        },{
+          name:"Cloud Platform",
+          children:[
+            { 
+              name:"GKE container"
+            },{
+                name:"Pub/Sub Client"
+              },{
+                name:"Spanner Database"
+              },{
+                name:"workload Management"
+              },{
+                name:"Memory cache"
+              },{
+                name:"Dataflows"
+              }
+          ]
+        },{
+          name:"Tools",
+          children:[
+            {
+              name:"version control tools",
+              children:[
+                {
+                  name:"Git"
+                },{
+                  name:"Gitlab"
+                },{
+                  name:"Github"
+                },{
+                  name:"bitbucket"
+                }
+              ]
+            },{
+              name:"Jenkins"
+            },{
+              name:"VS Code"
+            },{
+              name:"Browser developer tool"
             }
           ]
         }
@@ -106,10 +163,10 @@ const About = () => {
                             I have adoted an environment of agile methodologies, DevOps deliveries and client interaction.
                             </p>
                             <p>
-                            I was taught web development and designing by working in industrial projects and many times, myself 🤓.
-                            I also holds Bachelor of Engineering in Electronic from Mumbai University 📚.
+                            I was taught web development and designing by working in industrial projects and many times, myself <span role="img" aria-label="smart">🤓</span>.
+                            I also holds Bachelor of Engineering in Electronic from Mumbai University <span role="img" aria-label="books-university">📚</span>.
                             </p> 
-                            <p>I believe learning and accepting new logics, Is key to happiness 😉.</p>
+                            <p>I believe learning and accepting new logics, Is key to happiness <span role="img" aria-label="wink-emoji">😉</span>.</p>
                             </div> 
                             <div className="col-lg-6 order-1 order-lg-2 header-img">
                                 <div className="image about">
@@ -123,7 +180,7 @@ const About = () => {
         </section>
         <div className="skill__nodes">
             <h3 className="skill__heading">Skill-Tree.</h3>
-                <p>Dragable Component created using d3js-tree source. Sometimes I use D3js or Chartjs for representing the Data 📈.</p>
+                <p>Dragable Component created using D3js-tree source. <span role="img" aria-label="dataAnalysis">📈</span>.</p>
             <div id="treeWrapper" className= "treeWrapper_class" style={{ width: '100%', height: '30em' }} ref={treeContainer}>
                 <Tree data={debugData} translate={translate} orientation={'horizontal'}/>
             </div>
