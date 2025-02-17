58b5a1a9586e7f3a62928a037589fe2d0e0acec3
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2025-02-17 13:02:40 +0530

    remove link NAvlink

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+//import { Link, NavLink } from 'react-router-dom';
 import git from '../../Images/git.svg';
 import linkedin from '../../Images/link.svg';
 import medium from '../../Images/linkden2.svg';
